8a6f86519c4150c7a25798a214a4b0a6a51cd59b
DevGrg1213 <theyev1213@gmail.com>
2023-03-27 16:27:01 +0545

    created activity class and set activity on AndriodManifest file

---
+++ signup/app/src/main/java/com/divyagyan/signup/LoginFormActivity.java
@@ -0,0 +1,15 @@
+package com.divyagyan.signup;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class LoginFormActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__signupform);
+    }
+}
